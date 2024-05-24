9ebbd50182400a3e0bf63e8f08fb26b4f9122782
Anton-Osip <osipchika@gmail.com>
2024-05-24 19:09:16 +0300

    feat: Designed the structure of the Services section

---
+++ src/layout/sections/services/Services.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import services1 from '../../../assets/images/services/services-1.jpg'
+import services2 from '../../../assets/images/services/services-2.jpg'
+import services3 from '../../../assets/images/services/services-3.jpg'
+import services4 from '../../../assets/images/services/services-4.jpg'
+import services5 from '../../../assets/images/services/services-5.jpg'
+import services6 from '../../../assets/images/services/services-6.jpg'
+import { FlexWrapper } from '../../../components/FlexWrapper'
+import { ServicesItem } from './servicesItem/ServicesItem'
+const data = [
+	{ id: 'services6', src: services6, text: 'Парикмахерские услуги' },
+	{ id: 'services4', src: services4, text: 'Маникюр' },
+	{ id: 'services2', src: services2, text: 'Педикюр' },
+	{ id: 'services5', src: services5, text: 'Косметология' },
+	{ id: 'services3', src: services3, text: 'Эстетист по телу' },
+	{ id: 'services1', src: services1, text: 'Визаж' },
+]
+export function Services() {
+	return (
+		<StyledServices>
+			<FlexWrapper wrap='wrap' justify='space-between'>
+				{data.map(item => (
+					<ServicesItem item={item} />
+				))}
+			</FlexWrapper>
+		</StyledServices>
+	)
+}
+
+const StyledServices = styled.section`
+	min-height: 100vh;
+	background-color: #c2f9a4;
+`
---
+++ src/layout/sections/services/servicesItem/ServicesItem.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+type ServicesItemPropsType = { id: string; src: string; text: string }
+export function ServicesItem(props: { item: ServicesItemPropsType }) {
+	return (
+		<StyledServicesItem key={props.item.id}>
+			<Image src={props.item.src} alt={props.item.text} />
+			<Text>{props.item.text}</Text>
+		</StyledServicesItem>
+	)
+}
+
+const StyledServicesItem = styled.div`
+	width: 30%;
+	background-color: #f9a4a4;
+	margin: 10px;
+`
+const Image = styled.img`
+	width: 100%;
+`
+const Text = styled.h3``
