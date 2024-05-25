8be985d7e0182db4b4ba557d5443f8d534463847
Anton-Osip <osipchika@gmail.com>
2024-05-25 12:37:31 +0300

    feat:creat page HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import './App.css'
-import { Footer } from './layout/footer/Footer'
-import { Header } from './layout/header/Header'
-import { About } from './layout/sections/about/About'
-import { Cosmetics } from './layout/sections/cosmetics/Cosmetics'
-import { Main } from './layout/sections/main/Main'
-import { Services } from './layout/sections/services/Services'
-import { Works } from './layout/sections/works/Works'
+import {HomePage} from "./Pages/homePage/HomePage";
+import {Header} from "./layout/header/Header";
+import {Footer} from "./layout/footer/Footer";
+
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
-			<Main />
-			<About />
-			<Services />
-			<Cosmetics />
-			<Works />
+			<HomePage/>
 			<Footer />
+
 		</div>
 	)
 }
