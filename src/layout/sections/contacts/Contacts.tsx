8d46539c19fbd8603b02162107a4c713817926a2
Anton-Osip <osipchika@gmail.com>
2024-06-05 17:23:31 +0300

    feat: add adaptive for Contacts section

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -5,6 +5,7 @@ import { FlexWrapper } from '../../../components/FlexWrapper'
 import { SectionTitle } from '../../../components/SectionTitle'
 import { Contact } from '../../../components/contact/Contact'
 import { OperatingMode } from '../../../components/operatingMode/OperatingMode'
+import { theme } from '../../../styles/Theme'
 
 export function Contacts() {
 	return (
@@ -12,7 +13,12 @@ export function Contacts() {
 			<Container>
 				<SectionTitle>Контакты</SectionTitle>
 				<Image src={map} alt='map' />
-				<FlexWrapper align='flex-start' justify='space-between'>
+				<FlexWrapper
+					align='flex-start'
+					justify='space-around'
+					wrap='wrap'
+					gap='20'
+				>
 					<Contact />
 					<OperatingMode />
 					<Contact />
@@ -25,9 +31,32 @@ export function Contacts() {
 
 const StyledContacts = styled.section`
 	padding: 180px 0 100px;
+	@media ${theme.media.tablet} {
+		padding: 180px 0 65px;
+	}
+	@media ${theme.media.mobile} {
+		padding: 180px 0 30px;
+	}
+
+	${FlexWrapper} {
+		@media ${theme.media.tablet} {
+			gap: 30px;
+		}
+		@media ${theme.media.mobile} {
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: center;
+		}
+	}
 `
 
 const Image = styled.img`
 	width: 100%;
 	margin-bottom: 80px;
+	@media ${theme.media.tablet} {
+		margin-bottom: 60px;
+	}
+	@media ${theme.media.mobile} {
+		margin-bottom: 40px;
+	}
 `
