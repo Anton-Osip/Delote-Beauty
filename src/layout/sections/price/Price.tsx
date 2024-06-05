0db9c592b9eb747adecdcad716c885842bdfe2b6
Anton-Osip <osipchika@gmail.com>
2024-06-05 17:15:00 +0300

    feat: add adaptive for Price section

--- src/layout/sections/price/Price.tsx
+++ src/layout/sections/price/Price.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import img from '../../../assets/images/prices/price.jpg'
 import { Container } from '../../../components/Container'
 import { SectionTitle } from '../../../components/SectionTitle'
+import { theme } from '../../../styles/Theme'
 import { PriceList } from './priceList/PriceList'
 
 export default function Price() {
@@ -26,14 +27,25 @@ export default function Price() {
 
 const StyledPrice = styled.section`
 	padding: 180px 0 100px;
+	@media ${theme.media.tablet} {
+		padding: 180px 0 65px;
+	}
+	@media ${theme.media.mobile} {
+		padding: 180px 0 30px;
+	}
 `
 const PriceWrapper = styled.div`
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
 	column-gap: 24px;
 	row-gap: 80px;
+
+	@media screen and (max-width: 1168px) {
+		grid-template-columns: repeat(1, 1fr);
+	}
 `
 
 const Image = styled.img`
+	width: 100%;
 	aspect-ratio: 636/536;
 `
