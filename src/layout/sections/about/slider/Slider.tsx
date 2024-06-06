88d44d8f91ab002cbec2059e3b3ffc2b55769c6d
Anton-Osip <osipchika@gmail.com>
2024-06-06 20:46:21 +0300

    ref:refactoring About

--- src/layout/sections/about/slider/Slider.tsx
+++ src/layout/sections/about/slider/Slider.tsx
@@ -1,59 +1,22 @@
-import styled from 'styled-components'
-import { font } from '../../../../styles/Common'
-import { theme } from '../../../../styles/Theme'
+import { S } from '../About_Styles'
 
-export function Slider() {
+export const Slider: React.FC = () => {
 	return (
-		<StyledSlider>
-			<Slide>
-				<Text>
+		<S.Slider>
+			<S.Slide>
+				<S.Text>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In arcu nibh
 					vitae amet. Ipsum, pharetra donec ornare velit. Id at quisque accumsan
 					risus ac ipsum ut. Sit elit, facilisi proin non malesuada sociis
 					tristique. Viverra augue lorem ut quisque quam tortor, malesuada
 					iaculis.
-				</Text>
-				<Text>
+				</S.Text>
+				<S.Text>
 					Et elementum at nulla venenatis, faucibus integer. Auctor neque eros,
 					viverra rutrum. Fames ultrices condimentum tortor nec penatibus. Velit
 					imperdiet sapien fringilla vestibulum sit fames.
-				</Text>
-			</Slide>
-		</StyledSlider>
+				</S.Text>
+			</S.Slide>
+		</S.Slider>
 	)
 }
-
-const StyledSlider = styled.div`
-	max-width: 856px;
-	width: 100%;
-
-	margin-top: 60px;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-
-	@media ${theme.media.tablet} {
-		margin-top: 20px;
-	}
-`
-const Slide = styled.div`
-	text-align: center;
-`
-const Text = styled.p`
-	${font({
-		family: 'Cormorant Garamond',
-		weight: 700,
-		lineHeight: 1.3,
-		Fmax: 27,
-		Fmin: 20,
-	})}
-	letter-spacing: 0%;
-	text-align: center;
-
-	& + p {
-		margin-top: 30px;
-		@media ${theme.media.tablet} {
-			margin-top: 15px;
-		}
-	}
-`
