230c49ba31239a07572e10bf9e4be7d25de60828
Anton-Osip <osipchika@gmail.com>
2024-06-06 20:46:37 +0300

    ref:refactoring Contacts

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,18 +1,17 @@
-import styled from 'styled-components'
 import map from '../../../assets/images//map.jpg'
 import { Container } from '../../../components/Container'
 import { FlexWrapper } from '../../../components/FlexWrapper'
 import { SectionTitle } from '../../../components/SectionTitle'
 import { Contact } from '../../../components/contact/Contact'
 import { OperatingMode } from '../../../components/operatingMode/OperatingMode'
-import { theme } from '../../../styles/Theme'
+import { S } from './Contacts_styles'
 
-export function Contacts() {
+export const Contacts: React.FC = () => {
 	return (
-		<StyledContacts>
+		<S.Contacts>
 			<Container>
 				<SectionTitle>Контакты</SectionTitle>
-				<Image src={map} alt='map' />
+				<S.Image src={map} alt='map' />
 				<FlexWrapper
 					align='flex-start'
 					justify='space-around'
@@ -25,38 +24,6 @@ export function Contacts() {
 					<OperatingMode />
 				</FlexWrapper>
 			</Container>
-		</StyledContacts>
+		</S.Contacts>
 	)
 }
-
-const StyledContacts = styled.section`
-	padding: 180px 0 100px;
-	@media ${theme.media.tablet} {
-		padding: 180px 0 65px;
-	}
-	@media ${theme.media.mobile} {
-		padding: 180px 0 30px;
-	}
-
-	${FlexWrapper} {
-		@media ${theme.media.tablet} {
-			gap: 30px;
-		}
-		@media ${theme.media.mobile} {
-			flex-direction: column;
-			justify-content: flex-start;
-			align-items: center;
-		}
-	}
-`
-
-const Image = styled.img`
-	width: 100%;
-	margin-bottom: 80px;
-	@media ${theme.media.tablet} {
-		margin-bottom: 60px;
-	}
-	@media ${theme.media.mobile} {
-		margin-bottom: 40px;
-	}
-`
