69e9920f9589b19f88a1cf061d943b833f33b189
Anton-Osip <osipchika@gmail.com>
2024-06-06 20:47:14 +0300

    ref:refactoring Main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,68 +1,14 @@
-import styled from 'styled-components'
-import image from '../../../assets/images/baner2x.jpg'
-import { font } from '../../../styles/Common'
-import { theme } from '../../../styles/Theme'
+import { S } from './Main_styles'
 
-export function Main() {
+export const Main: React.FC = () => {
 	return (
-		<StyledMain>
-			<MainTitle>
+		<S.Main>
+			<S.MainTitle>
 				Салон красоты <br />
 				«DELOTE BEAUTY»
 				<br /> на Крестовском
-			</MainTitle>
-			<ScrollDown>Прокрутите вниз</ScrollDown>
-		</StyledMain>
+			</S.MainTitle>
+			<S.ScrollDown>Прокрутите вниз</S.ScrollDown>
+		</S.Main>
 	)
 }
-
-const StyledMain = styled.div`
-	background: rgba(0, 0, 0, 0.5) url(${image}) no-repeat center / cover;
-	padding: 100px 0 5%;
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	position: relative;
-	background-blend-mode: multiply;
-`
-const MainTitle = styled.h1`
-	text-align: center;
-	flex-grow: 2;
-	display: flex;
-	align-items: center;
-	${font({ weight: 700, lineHeight: 1.2, Fmax: 72, Fmin: 40 })}
-`
-
-const ScrollDown = styled.button`
-	font-family: 'Cormorant Garamond';
-	display: inline-block;
-	padding-left: 40px;
-	position: relative;
-	font-size: 18px;
-	font-weight: 700;
-	line-height: 140%;
-	&:after {
-		content: '';
-		position: absolute;
-		top: -5px;
-		left: 0;
-		display: inline-block;
-		width: 20px;
-		height: 30px;
-		border: 1px solid ${theme.colors.font};
-		border-radius: 8px;
-	}
-	&::before {
-		content: '';
-		position: absolute;
-		top: 0px;
-		left: 8px;
-		display: inline-block;
-		width: 4px;
-		height: 4px;
-		border-radius: 50%;
-		background-color: ${theme.colors.font};
-	}
-`
---
+++ src/layout/sections/main/Main_styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+import image from '../../../assets/images/baner2x.jpg'
+import { font } from '../../../styles/Common'
+import { theme } from '../../../styles/Theme'
+
+const Main = styled.div`
+	background: rgba(0, 0, 0, 0.5) url(${image}) no-repeat center / cover;
+	padding: 100px 0 5%;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	position: relative;
+	background-blend-mode: multiply;
+`
+const MainTitle = styled.h1`
+	text-align: center;
+	flex-grow: 2;
+	display: flex;
+	align-items: center;
+	${font({ weight: 700, lineHeight: 1.2, Fmax: 72, Fmin: 40 })}
+`
+
+const ScrollDown = styled.button`
+	font-family: 'Cormorant Garamond';
+	display: inline-block;
+	padding-left: 40px;
+	position: relative;
+	font-size: 18px;
+	font-weight: 700;
+	line-height: 140%;
+	&:after {
+		content: '';
+		position: absolute;
+		top: -5px;
+		left: 0;
+		display: inline-block;
+		width: 20px;
+		height: 30px;
+		border: 1px solid ${theme.colors.font};
+		border-radius: 8px;
+	}
+	&::before {
+		content: '';
+		position: absolute;
+		top: 0px;
+		left: 8px;
+		display: inline-block;
+		width: 4px;
+		height: 4px;
+		border-radius: 50%;
+		background-color: ${theme.colors.font};
+	}
+`
+
+export const S = { Main, MainTitle, ScrollDown }
